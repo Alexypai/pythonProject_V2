805e9b9e984f44bed837efbe3a282ba58c6dc427
Alexypai <72445670+Alexypai@users.noreply.github.com>
2021-01-18 19:19:15 +0100

    Add files via upload

---
+++ Gestionnaire version 2 (python project)/Gestionnaire version 2 (python project)/data.sql
@@ -0,0 +1,19 @@
+INSERT INTO USERS (USERS_NAME,TAUX_HORAIRE,BIRTHDAY,MAIL,CONTACT_NUMBER,ENTITY,password,Responsibilities_Name)
+VALUES
+('Féret','35','01/02/1988','Féret@orange.com','600203554','Orange/OF/DTSI/DTRS/UPRSE/ID2R/GOP', 1231,"RH"),
+('Gide','20','11/06/1980','Gide@orange.com','615483554','Orange/OF/DTSI/DTRS/UPRNE/ID2R', 1232,"MANAGER"),
+('Vaillancourt','30','19/12/1990','Vaillancourt@orange.com','600201975','Orange/OF/DTSI/DTRS/UPRSE', 1233,"DEVELOPER"),
+('Laurens','40','31/07/1978','Laurens@orange.com','604723554','Orange/OF', 1234,"MANAGER"),
+('Benett','15','01/02/2000','Benett@orange.com','614586354','Orange/OF/DTSI/DTRS/UPRSE/ID2R', 1235,"ASSISTANT"),
+('Alméras','27','25/10/1970','Alméras@orange.com','789303554','Orange/OF/DTSI/DTRS', 1236,"COMMERCIAL"),
+('Charbonneau','30','01/04/1995','Charbonneau@orange.com','600147854','Orange/OF/DTSI/DTRS/UPRSE/ID2R/GOP', 1237,"DEVELOPER"),
+('Bullion','35','22/02/1969','Bullion@orange.com','614523654','Orange/OF/DTSI', 1238,"RH"),
+('Dumont','20','19/08/1992','Dumont@orange.com','645693554','Orange/OF/DTSI/DTRS/UPRSE/ID2R/GOP', 1239,"ASSISTANT"),
+('Lussier','40','25/01/1997','Lussier@orange.com','614783654','Orange/OF/DTSI/DTRS', 1240,"COMMERCIAL"),
+('Cazenave','29','04/05/1970','Cazenave@orange.com','789303554','Orange/OF/DTSI/DTRS', 1241,"COMMERCIAL"),
+('Génin','45','30/06/1982','Génin@orange.com','600185254','Orange/OF/DTSI/DTRS/UPRSE/ID2R/GOP', 1242,"DEVELOPER"),
+('Leclère','17','23/02/1999','Leclère@orange.com','756423654','Orange/OF/DTSI', 1243,"ASSISTANT"),
+('Beauchamp','30','19/11/1962','Beauchamp@orange.com','645691487','Orange/OF/DTSI/DTRS/UPRSE', 1244,"MANAGER");
+
+INSERT INTO RESPONSIBILITIES (Responsibilities_Name)
+VALUES ("RH"),("MANAGER"),("DEVELOPER"),("ASSISTANT"),("COMMERCIAL");
